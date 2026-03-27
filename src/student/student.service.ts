7b621c42c063d49a43c88c90c739652ee645f26b
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2026-03-27 14:12:27 +0600

    student api created

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  private students = [
+    { id: 1, name: 'Fawza', age: 20 },
+    { id: 2, name: 'A', age: 21 },
+    { id: 3, name: 'Takiya', age: 22 },
+  ];
+
+  getAllStudents() {
+    return this.students;
+  }
+
+  getById(id: number) {
+    return this.students.find((student) => student.id === id);
+  }
+
+  createStudent(data: { name: string; age: number }) {
+    const newStudent = {
+      id: Date.now(),
+      ...data,
+    };
+    this.students.push(newStudent);
+    return newStudent;
+  }
+
+  updateStudent(id: number, data: { name: string; age: number }) {
+    const student = this.students.find((st) => st.id === id);
+    if (!student) {
+      return new NotFoundException(`Student with id ${id} not found`);
+    }
+    Object.assign(student, data);
+    return student;
+  }
+
+  patchStudent(id: number, data: Partial<{ name: string; age: number }>) {
+    const student = this.getById(id);
+    if (!student) {
+      return new NotFoundException('stundent not found');
+    }
+    Object.assign(student, data);
+  }
+
+  deleteStudent(id: number) {
+    const student = this.getById(id);
+    if (!student) {
+      return new NotFoundException('stundent not found');
+    }
+
+    const deleted = this.students.splice(
+      this.students.findIndex((st) => st.id === id),
+      1,
+    );
+    return deleted;
+  }
+}
