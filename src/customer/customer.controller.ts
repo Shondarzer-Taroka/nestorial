3332eb2dc3454dfd83c7973ef45f8f1b5aa5a664
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2026-03-31 21:44:20 +0600

    get and create api built

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { Customer } from './interfaces/customer-interface';
+import { CreateCustomerDto } from './dto/create-customer-dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+  @Post()
+  createCustomer(@Body() createCustomerdto: CreateCustomerDto) {
+    this.customerService.createCustomer(createCustomerdto);
+  }
+
+  @Get()
+  getAllCustomers(): Customer[] {
+    return this.customerService.getAllCustomers();
+  }
+}
